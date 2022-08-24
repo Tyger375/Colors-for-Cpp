8790c68edcc54a4543186b1f9c107cd30427a4c9
Tyger375 <edoardo.takanen@gmail.com>
2022-08-24 17:07:52 +0200

    Added progress bars

--- Colors.h
+++ Colors.h
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <stdio.h>
 #include <string>
 #include <vector>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
 
 using namespace std;
 
@@ -27,7 +33,7 @@ namespace Styles
 
 namespace Codes
 {
-    //If your terminal supports colors and it isn't here, please, add its name
+    //If your terminal supports colors and it isn't here, please, add its name and make a pull request
     //You can see your terminal's name by debugging the "term" variable
     vector<string> validTerminals = {
         "xterm",
@@ -54,6 +60,7 @@ namespace Codes
         }
         return found;
     };
+
     string prefix = "\033[";
     string end = "m";
     string String(int code) { return can_show_colors() ? (prefix + to_string(code) + end) : ""; };
@@ -95,3 +102,53 @@ namespace Codes
         return final_str;
     }
 };
+
+class ProgressBar
+{
+public:
+   float progress;
+   int goal;
+   string strinfos;
+   char progress_char;
+   string color;
+   ProgressBar(char c=' ', int goal=10, string info="", string Color=Codes::background(Colors::GREEN))
+   {
+	this->progress = 0;
+	this->goal = goal;
+	progress_char = c;
+	this->strinfos = info;
+	this->color=Color;
+   };
+   int getPercentage() {
+	float divided = (float)(progress / (float)this->goal);
+	return (int)(divided*100);
+   };
+   void infos()
+   {
+	printf("%s: ", strinfos.c_str());
+   }
+   void debug()
+   {
+	printf("\033[0m");
+	for (int i = 100; i > getPercentage(); i--)
+	{
+	   printf(" ");
+	}
+	printf("\t\t\t%d/%d - %d%%", (int)this->progress, this->goal, getPercentage());
+   }
+   void update(float increment)
+   {
+	printf("\r");
+	this->infos();
+	printf("%s", color.c_str());
+	for (int j = 0; j < getPercentage(); j++)
+	{
+           printf("%c", this->progress_char);
+	}
+	this->debug();
+	printf("\033[0m");
+	this->progress += increment;
+	fflush(stdout);
+   };
+};
+
